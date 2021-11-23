304a75aab3cec86b3721558f22b0bfbaf4e113e9
gopitk <gokuma@microsoft.com>
2021-11-23 21:27:58 +0000

    Avoid error at end of training

--- tools/train.py
+++ tools/train.py
@@ -198,11 +198,6 @@ def main():
         model, args.distributed, final_output_dir, 'final_state.pth'
     )
 
-    if config.SWA.ENABLED and comm.is_main_process():
-        save_model_on_master(
-             args.distributed, final_output_dir, 'swa_state.pth'
-        )
-
     writer_dict['writer'].close()
     logging.info('=> finish training')
 
